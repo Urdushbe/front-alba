a7ccb0e92bc1fed2b62c490ed752b79344c646ea
Kuysinboev Urdushbek <Urdushbek.official@gmail.com>
2023-04-24 15:31:16 +0900

    Job-info created

--- src/pages/JobInfoPage.vue
+++ src/pages/JobInfoPage.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="row">
-    <div class="col-12">
-      <h1>Kuysinboev Urdushbek</h1>
-      <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin
+    <div
+        v-for="job of getJobs"
+        :key="job.id"
+        class="col-12">
+      <h1>{{job.name}}</h1>
+      <p>{{job.text}}
       </p>
+        <p>
+            Ishchi soni: {{ job.person }} kishi<br />
+            Manzil: {{ job.address }}<br />
+            Telefon raqam: {{ job.tel }}<br />
+            Bolim: {{ job.category }}
+        </p>
     </div>
   </div>
 </template>
@@ -11,8 +20,25 @@
 <script>
 
 
+import {mapActions, mapGetters} from "vuex";
+
 export default {
   name: "JobInfoPage",
+    methods: {
+        ...mapActions(['fetchJobs'])
+    },
+    computed: {
+        ...mapGetters(['getJobs'])
+    },
+    mounted() {
+        this.fetchJobs(this.$route.params.id)
+    },
+    watch: {
+        '$route.params.id'(){
+            console.log('watch ishladi')
+            this.fetchJobs(this.$route.params.id)
+        }
+    }
 
 }
 </script>
